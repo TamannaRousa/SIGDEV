4b50553249fbb20fdea3d5447bff885d487d2f15
TamannaRousa <tamanna.rousa.cs25@iilmcet.ac.in>
2024-08-11 23:47:22 +0530

    javaScript basics

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -50,12 +50,107 @@ switch(colour){
    case "red":
     console.log("STOP!");
     break;
-  case "yellow": {
+  case "yellow": 
     console.log("READY!");
     break;
-  case "green": {
+  case "green": 
     console.log("GO!");
     break;
   default:
     console.log("NO RULE!");
 }
+
+console.log("after switch stt");
+
+let day = 1;
+switch(day){
+    case 1:
+    console.log("Monday");
+    break;
+    case 2: 
+    console.log("Tuesday");
+    break;
+    case 3: 
+    console.log("Wednesday");
+    break;
+    case 4: 
+    console.log("Thursday");
+    break;
+    case 5: 
+    console.log("Friday");
+    break;
+    case 6: 
+    console.log("Saturday");
+    break;
+    case 7: 
+    console.log("Sunday");
+    break;
+    default:
+    console.log("NO RULE!");
+}
+//null
+let abc = null;//have to define null 
+//undefined
+let abc1 ;//need not to define
+
+//concatenation=+
+console.log("abscsd"+1);
+
+//string length
+let firsname = "Tamanna";
+console.log(firsname.length);
+console.log(firsname[3]);
+
+//truthy-value(except falsy everything has true values) & falsy-value(0,-0,"",undefined,null)
+if(true){
+    console.log("true values");
+}
+else{
+    console.log("flase values");
+}
+
+//alerts & errors & warning  in JS
+alert("Warning!");
+console.error("ERROR !");
+console.warn("WARNING!");
+
+//prompt
+let firname = prompt("enter name");
+console.log(firname);
+
+//largest of 3 numbers
+let x =523;
+let y=34;
+let z=240;
+
+if(x>y){
+    if(y>z){
+    console.log("x is greatest");
+    }
+    else{
+        console.log("z is greatest");  
+    }
+}
+else if(y>z){
+    if(z>x){
+        console.log("y is greatest");
+    }
+    else{
+        console.log("z is greatest");
+    }
+}
+//question
+let name = prompt("enter name");
+alert(`${name}`);
+
+//string methods - toLowerCase & toUpperCase
+//with arguements
+
+let msg = "    IloveIndex";
+msg.trim();
+//msg.trim().toUpperCase();
+msg.indexOf("love");
+msg.indexOf("l");
+msg.slice(1,6);
+msg.replace("love","do");
+msg.repeat(3);
